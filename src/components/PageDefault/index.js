c4d8d7d3c2f2dc423384b550941ab572bdab14ac
Anderson <loyolaandersom2013@gmail.com>
2020-08-04 16:43:44 -0300

    Add modificações da aula-03

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Menu from '../Menu';
 import Footer from '../Footer';
+
 const Main = styled.main`
     background-color: var(--black);
     color: var(--white);
